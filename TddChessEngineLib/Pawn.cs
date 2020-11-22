af7b6b8ada0d1b1f035b241e7028eb26762f5eb3
TheMuver <via.zyn@ya.ru>
2020-11-22 11:57:08 +0300

    Add figure color

--- TddChessEngineLib/Pawn.cs
+++ TddChessEngineLib/Pawn.cs
@@ -2,13 +2,22 @@
 
 namespace TddChessEngineLib
 {
+    public enum FigureColor { White, Black }
+
     public class Pawn
     {
-        public string Position {get; private set;}
+        public string CurrentPosition {get; private set;}
+        public FigureColor FigureColor { get; }
+
+        public Pawn (string initPosition, FigureColor color)
+        {
+            CurrentPosition = initPosition;
+            FigureColor = color;
+        }
 
         public void TurnTo(string finishPosition)
         {
-            Position = finishPosition;
+            CurrentPosition = finishPosition;
         }
     }
 }
