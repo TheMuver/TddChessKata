d9585835532c870f120d82a43a698bc9eb2432d5
TheMuver <via.zyn@ya.ru>
2020-12-06 11:53:17 +0300

    small update in tests

--- TddChessEngineLibTests/ElephantTests.cs
+++ TddChessEngineLibTests/ElephantTests.cs
@@ -6,28 +6,28 @@ namespace TddChessEngineLibTests
 {
     public class ElephantTests
     {
-        const string e2Position = "E2";
-        const string e9Position = "E9";
-        const string p2Position = "P2";
+        const string E2Position = "E2";
+        const string E9Position = "E9";
+        const string P2Position = "P2";
         
         [Fact]
         public void WhenCreatingElephantOnE2_ThenItCurrentPositionE2()
         {
-            Elephant elephant = new Elephant(e2Position);
+            Elephant elephant = new Elephant(E2Position);
 
-            Assert.Equal(e2Position, elephant.CurrentPosition);
+            Assert.Equal(E2Position, elephant.CurrentPosition);
         }
 
         [Fact]
         public void WhenTryCreatingElephantOnE9_ThenItThrowsArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => new Elephant(e9Position));
+            Assert.Throws<ArgumentException>(() => new Elephant(E9Position));
         }
 
         [Fact]
         public void WhenTryCreatingElephantOnP2_ThenItThrowsArgumentException()
         {
-            Assert.Throws<ArgumentException>(() => new Elephant(p2Position));
+            Assert.Throws<ArgumentException>(() => new Elephant(P2Position));
         }
     }
 }
