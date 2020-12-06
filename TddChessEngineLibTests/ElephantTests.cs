5fc71c3dcbefa6e973048f072316b96dd0e9ed69
TheMuver <via.zyn@ya.ru>
2020-12-06 11:40:52 +0300

    new test (elephant tries be on E9)

--- TddChessEngineLibTests/ElephantTests.cs
+++ TddChessEngineLibTests/ElephantTests.cs
@@ -7,18 +7,19 @@ namespace TddChessEngineLibTests
     public class ElephantTests
     {
         const string e2Position = "E2";
+        const string e9Position = "E9";
         
         [Fact]
         public void WhenCreatingElephantOnE2_ThenItCurrentPositionE2()
         {
-            Elephant elephant = new Elephant(E2);
+            Elephant elephant = new Elephant(e2Position);
 
             Assert.Equal(e2Position, elephant.CurrentPosition);
         }
 
         public void WhenTryCreatingElephantOnE9_ThenItThrowsArgumentException()
         {
-
+            Assert.Throws<ArgumentException>(() => new Elephant(e9Position));
         }
     }
 }
