af763ae86c24eb5bef46bd6ce4e0c7e381b9c3ba
TheMuver <via.zyn@ya.ru>
2020-12-06 12:05:39 +0300

    update elephant class

--- TddChessEngineLib/Elephant.cs
+++ TddChessEngineLib/Elephant.cs
@@ -10,10 +10,18 @@ namespace TddChessEngineLib
         public Elephant(string position) 
         {
             if (position[1] == '9')
-                throw new ArgumentException("Elephant can't created on E9");
+                throw new ArgumentException("Elephant can't created there");
             else if (position[0] == 'P')
-                throw new ArgumentException("Elephant can't created on P2");
+                throw new ArgumentException("Elephant can't created there");
             this.CurrentPosition = position;   
+        }
+
+        public void TurnTo(string newPosition)
+        {
+            if (CurrentPosition == "E2" && newPosition == "E3")
+                throw new ArgumentException("Elephant can't go there");
+            else if (CurrentPosition == "E2" && newPosition == "F3")
+                throw new ArgumentException("Elephant can't go there");
         }
     }
 }
